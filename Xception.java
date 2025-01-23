2d8f3e9b797ac8ca7b45aa1dcbe6c85f4d7b7159
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-23 14:11:20 +0530

    Add files via upload

---
+++ Xception.java
@@ -0,0 +1,23 @@
+
+package xception;
+class Ageex extends Exception{
+    Ageex(String msg){
+        super(msg);
+        }
+    }
+public class Xception 
+{
+    public static void main(String[] args)
+    {        
+        try {
+            int x=9;
+            if(x<20) throw new Ageex("Age < 18. Not allowed!");
+            System.out.println("Allowed!");
+        }
+        catch(Ageex e)
+        {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
