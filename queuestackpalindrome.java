2e0e468ae82e59ff020be8b8dfe80112ce5811a1
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-27 13:58:50 +0530

    Create queuestackpalindrome.java

---
+++ queuestackpalindrome.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.*;
+
+public class Prq {
+    public static void main(String [ ] args){
+    Queue<Integer>  pq  = new LinkedList<>();
+    Stack<Integer> st = new Stack<>();
+    int x = 12821;
+    while(x>0){
+        int n=x%10;
+        pq.offer(n);
+        st.add(n);
+        x/=10;
+    }
+    int flag=1;
+    while(!pq.isEmpty()){
+        if(pq.poll() != st.pop()) {
+            flag = 0;
+            break;
+        }
+    }
+    System.out.println(flag);
+    }
+}
