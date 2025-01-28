09d6f336c35c5dd275efd6395be0475b93f24d59
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-28 14:40:01 +0530

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 
 public class Mobile {
 	public static void main(String[] args) {
-		String t = "+91 97895 86798";
-		if(Pattern.matches("\\+[0-9]{1,3}\\s[6-9]....\\s[0-9]{5}", t)) System.out.println("tamil number");
+		String t = "+91 97695 86798";
+		if(Pattern.matches("\\+[0-9]{1,3}\\s[6-9]{1}[0-9]{4}\\s[0-9]{5}", t)) System.out.println("tamil number");
 		else  System.out.println("other state");
 	}
 }
