cd577ca982f7d486cf0638b43a42057c831f3ac6
Aashif786 <aashifanshaf786@gmail.com>
2025-02-08 13:13:55 +0530

    armstrong no updated

--- Bheem.java
+++ Bheem.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Bheem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range : ");
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             if (isArms(i))
@@ -14,9 +15,10 @@ public class Bheem {
     public static boolean isArms(int n) {
         int x = n;
         int sum = 0;
+        int len = String.valueOf(x).length();
         while (x > 0) {
             int r = x % 10;
-            sum = sum + (int) Math.pow(r, 2);
+            sum = sum + (int) Math.pow(r, len);
             x = x / 10;
         }
         return sum == n;
