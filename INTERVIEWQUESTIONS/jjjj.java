aecef7ec6137fb9e6420cc5746d2a55da5bc08f6
Aashif786 <aashifanshaf786@gmail.com>
2025-08-16 14:33:31 +0530

    Repo changes

--- INTERVIEWQUESTIONS/jjjj.java
+++
@@ -1,7 +0,0 @@
-package INTERVIEWQUESTIONS;
-
-public class jjjj {
-    public static void main(String[] args) {
-        System.out.println(10 + 20 + "Java" + 10 + 20);
-    }
-}
--- Top300Training/SegmentTree.java
+++
@@ -1,8 +0,0 @@
-public class SegmentTree {
-    static int querySegment = 0;
-    public static void main(String[] args) {
-        int [] a = {9,1,5 ,2,6,8};
-        int n = a.length;
-    }
-
-}
